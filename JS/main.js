67075a36e76112c835c8823f36c8f949d9241f57
juanyr <juanyrivas2006@cesanrafael.org>
2024-11-19 14:19:57 -0400

    final

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+let mensaje="Hola Mundo";
+console.log(mensaje);
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultadoSuma= numerouno+numerodos+numerotres;
+console.log("la suma es ",resultadoSuma);
+let resultadoResta= numerouno-numerodos-numerotres;
+console.log("la resta es ",resultadoResta);
+let resultadoMultiplicacion= numerouno*numerodos*numerotres;
+console.log("la multiplicacion es ",resultadoMultiplicacion);
+let resultadoDivision= numerouno/numerodos/numerotres;
+console.log("la division es ",resultadoDivision);
+function Sumar(){
+    let numerouno=4;
+    let numerodos=6;
+    let numerotres=3;
+    let resultado= numerouno+numerodos+numerotres;
+    alert("la suma es"+ resultado);
+}
+function Restar(){
+    let numerouno=4;
+    let numerodos=6;
+    let numerotres=3;
+    let resultado= numerouno-numerodos-numerotres;
+    alert("la resta es"+ resultado);
+}
+function Multiplicar(){
+    let numerouno=4;
+    let numerodos=6;
+    let numerotres=3;
+    let resultado= numerouno*numerodos*numerotres;
+    alert("la multiplicacion es"+ resultado);
+}
+function Dividir(){
+    let numerouno=4;
+    let numerodos=6;
+    let numerotres=3;
+    let resultado= numerouno/numerodos/numerotres;
+    alert("la division es"+ resultado);
+}
