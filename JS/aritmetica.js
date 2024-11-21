4b483fb573cdc224cc467ce0aaed75498a43c569
juanyr <juanyrivas2006@cesanrafael.org>
2024-11-21 14:23:17 -0400

    final

---
+++ JS/aritmetica.js
@@ -0,0 +1,44 @@
+function sumar(){
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadosuma=parseFloat(valor1)+parseFloat(valor2);
+    if (valor1==""||valor2==""){
+        alert("El campo esta vacio")
+     }
+else
+{resultadoTotal.innerHTML= "El resultado es:" + resultadosuma;}
+}
+function restar(){
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadoresta=parseFloat(valor1)-parseFloat(valor2);
+ if (valor1==""||valor2==""){
+    alert("El campo esta vacio")
+ }
+else
+{resultadoTotal.innerHTML= "El resultado es:" + resultadoresta;}
+}
+function multiplicar(){
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadomultiplicacion=parseFloat(valor1)*parseFloat(valor2);
+ if (valor1==""||valor2==""){
+    alert("El campo esta vacio")
+ }
+else
+{resultadoTotal.innerHTML= "El resultado es:" + resultadomultiplicacion}
+}
+function dividir(){
+    let valor1=document.getElementById("dato1").value;
+    let valor2=document.getElementById("dato2").value;
+    let resultadoTotal=document.getElementById("resultado");
+    let resultadodivision=parseFloat(valor1)/parseFloat(valor2);
+ if (valor1==""||valor2==""){
+    alert("El campo esta vacio")
+ }
+else
+{resultadoTotal.innerHTML= "El resultado es:" + resultadodivision;}
+}
